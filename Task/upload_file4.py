c740ee7dd7c3d14b33f10c7b65c1ecf3eb52ea43
punaisxx <rawin.nx@gmail.com>
2024-06-25 12:38:42 +0700

    Upload raster file to database automatically

---
+++ Task/upload_file4.py
@@ -0,0 +1,43 @@
+import subprocess
+from datetime import datetime
+import os
+import shutil
+
+source_dir = '/data/raster_results'
+target_dir = '/used_data'
+
+tif_files = [f for f in os.listdir(source_dir) if f.endswith(('.tif', '.tiff'))]
+file_count = len(tif_files)
+print(f"Number of .tif files: {file_count}")
+
+if file_count > 0:
+  
+    # Get the current timestamp
+    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+
+    for tif_file in tif_files:
+        # Extract the filename without the extension
+        file_name = os.path.splitext(tif_file)[0]
+        
+        # Create the table name with timestamp
+        table_name = f"{file_name}_{timestamp}"
+
+        # Step 2: Run the raster2pgsql command for each file
+        # Construct the raster2pgsql command
+        command = raster2pgsql_command = f"raster2pgsql -s 4326 -I -C {os.path.join(source_dir, tif_file)} -F -t 100x100 public.{table_name} | psql -d postgres -U postgres -h 10.223.72.83 -p 5433"
+
+        # Execute the command using subprocess
+        try:
+                subprocess.run(command, shell=True, check=True)
+                print('Raster data imported successfully.')
+        except subprocess.CalledProcessError as e:
+                print(f'Error: Raster data import failed: {e}')
+
+        # Step 3: Move the processed file to the target directory
+        shutil.move(os.path.join(source_dir, tif_file), os.path.join(target_dir, tif_file))
+        print(f"{tif_file} has been moved to {target_dir}")
+
+        # os.remove(os.path.join(target_dir, tif_file))
+        # print(f"{tif_file} has been deleted from {target_dir}")
+else:
+    print("No .tif files found to process.")
