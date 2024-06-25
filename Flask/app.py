104ffe7b10305c26c919f71dd13cfe6aaba27d9c
punaisxx <rawin.nx@gmail.com>
2024-06-25 16:26:48 +0700

    Access the API test

--- Flask/app.py
+++ Flask/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
-# from flask_swagger_ui import get_swaggerui_blueprint
-import rasterio
-import requests
-from rasterio.mask import mask
-from shapely.geometry import Polygon, mapping
-import numpy as np
+from flask import Flask, request, jsonify
+# import rasterio
+# from rasterio.mask import mask
+# from shapely.geometry import Polygon, mapping
+# import numpy as np
+
+from lat_lon import lat_lon_to_band
 
 app = Flask(__name__)
 
@@ -15,5 +15,13 @@ tiff_file = '/home/kea-trainee-punpun/Week3/Flask/data/CPM_lstm_2023_2.tiff'
 def hello():
   return { "message": "Hello!" }
 
+@app.route('/api/crop', methods=['GET'])
+def get_crop():
+    file_path = '/data/raster_results/CPM_lstm_2023_new.tiff'
+    lon = float(request.args.get('lon'))
+    lat = float(request.args.get('lat'))
+    result = lat_lon_to_band(file_path, lon, lat)
+    return jsonify(result)
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8102, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- Flask/run_container_python.sh
+++ Flask/run_container_python.sh
@@ -1,6 +1,7 @@
 docker run -it \
---name punpun_python \
+--name punpun_flask \
 -p 8102:5000 \
--v /home/kea-trainee-punpun/Week3/Flask/:/app \
+-v /home/kea-trainee-punpun/git/Nectec_Trainee/Flask/:/app \
 -v /trainee_data/punpun_data:/app/data \
-python:3.10.12-slim bash
+-v /home/kea-trainee-punpun/DATA/raster_results:/data/raster_results \
+python:3.10.12-slim python
